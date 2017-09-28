3632ff700cf85e1765854a6553a38c88082e7985
pijey <pjvilloud-bitbucket@yahoo.fr>
2017-09-28 17:02:41 +0200

    Relecture à faire mais contenu global ok

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-impress().init();
+var imp = impress();
+imp.init();
 hljs.initHighlightingOnLoad();
 var nbSlides = $(".step.slide").length;
 var showPopover = new URL(window.location).searchParams.get("showPopover");
@@ -44,4 +45,36 @@ $(window).on('hashchange', function(e){
     $('#my-navbar li a[href="' + newHash.substring(0,newHash.lastIndexOf("-")) + '"]').addClass("active");
     $('#my-navbar li a[href="' + newHash + '"]').addClass("active");
     
+});
+
+$(window).on('contextmenu', function(e){
+  e.preventDefault();
+  imp.prev();
+});
+
+$(window).on('click', function(e){
+  if( (e.which == 2) ) {
+      e.preventDefault();
+      imp.next();
+   }
+});
+
+//-------------------
+
+$("#correctGeneric").click(function(event) {
+  $(event.target).toggleClass('btn-primary').toggleClass('btn-success');
+  $("#correctGeneric > i").toggleClass('fa-times-circle ');
+  $("#correctGeneric > i").toggleClass('fa-check-circle');
+  $("#genericCompareTo").toggleClass('text-danger').toggleClass('text-success');
+  var html = $("#genericExtends").html();
+  $("#genericExtends").html(html === "" ? " extends Comparable" : "");
+});
+
+$("#toggleClassIntern").click(function(event) {
+  $(event.target).toggleClass('btn-primary').toggleClass('btn-success');
+  $("#toggleClassIntern > i").toggleClass('fa-times-circle ');
+  $("#toggleClassIntern > i").toggleClass('fa-check-circle');
+  $("#interneNew").toggleClass('text-danger').toggleClass('text-success')
+  var html = $("#internePrivate").html();
+  $("#internePrivate").html(html === '<span class="hljs-keyword">private</span>' ? '<span class="hljs-keyword">public</span>' : '<span class="hljs-keyword">private</span>');
 });
