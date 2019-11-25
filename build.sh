9c287c344890af4b2d48c5c42d2c8946a755e82a
Pierre-Julien Villoud <pjvilloud@protonmail.com>
2019-11-25 11:23:27 +0100

    Update PDF and build script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p screenshots
 rm -rf screenshots/*.png
-decktape  --screenshots --screenshots-directory screenshots --screenshots-size 1920x1080 impress file://`pwd`/index.html?showPopover=true mysql_010_cours.pdf
+decktape  --screenshots --screenshots-directory screenshots --screenshots-size 1920x1080 impress file://`pwd`/index.html?showPopover=true java_220_cours.pdf
 rm java_220_cours.pdf
 cd screenshots
-convert `ls -1v` ../java_220_cours.pdf
+convert `ls` ../java_220_cours.pdf
 cd ..
